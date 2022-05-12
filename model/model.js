7d68e978ea571bfb431ebb3a052092aece6ad439
canh-pc <toicanh25@gmail.com>
2022-05-12 23:54:21 +0700

    Add model.js

---
+++ model/model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const authorSchema = mongoose.Schema({
+  name: {
+    type: String,
+  },
+  year: {
+    type: Number,
+  },
+  books: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Book",
+    },
+  ],
+});
+
+const bookSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  publishedDate: {
+    type: String,
+  },
+  genres: {
+    type: [String],
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+  },
+});
+
+let Book = mongoose.model("Book", bookSchema);
+
+module.exports = { Book };
