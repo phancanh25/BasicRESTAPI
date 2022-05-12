3f4d02a3a7e5b172a23009e2998c306efee03522
canh-pc <toicanh25@gmail.com>
2022-05-13 01:08:11 +0700

    Done GetAllAuthor

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -11,6 +11,14 @@ const authorController = {
       res.status(500).json(error);
     }
   },
+  getAllAuthors: async (req, res) => {
+    try {
+      const authors = await Author.find();
+      res.status(200).json(authors);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
 
 module.exports = authorController;
--- routes/author.js
+++ routes/author.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authorController = require("../controllers/authorController");
 
 router.post("/", authorController.addAuthor);
+router.get("/", authorController.getAllAuthors);
 
 module.exports = router;
