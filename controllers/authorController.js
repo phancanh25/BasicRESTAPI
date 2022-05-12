f9a433d81c40ccb748b7fb8c6a47d5ae4eefc259
canh-pc <toicanh25@gmail.com>
2022-05-13 00:45:19 +0700

    Done addAuthor

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -3,7 +3,13 @@ const { Author, Book } = require("../model/model");
 const authorController = {
   //add author
   addAuthor: async (req, res) => {
-    res.status(200).json(req.body);
+    try {
+      const newAuthor = new Author(req.body);
+      const saveAuthor = await newAuthor.save();
+      res.status(200).json(saveAuthor);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
 };
 
