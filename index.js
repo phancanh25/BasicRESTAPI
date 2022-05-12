99c86afd9b47c02be030a5c30174be5b4ebbfa2c
canh-pc <toicanh25@gmail.com>
2022-05-12 23:32:17 +0700

    Test app and connect to Alas database

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ var bodyParser = require("body-parser");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
+//Connect to db
+mongoose.connect(process.env.mongoosedbURL, () => {
+  console.log("Connect to database Sucessfully!!!");
+});
+
+app.get("/api", (req, res) => {
+  res.status(200).json("Hello World");
+});
+
 dotenv.config();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
